8adff7b512d00df50d9439103f9a09e18e332971
unknown <info@schedulize.co.ke>
2023-12-31 04:06:53 +0300

    Create Item

--- app/Livewire/TodoList.php
+++ app/Livewire/TodoList.php
@@ -2,12 +2,38 @@
 
 namespace App\Livewire;
 
+use App\Models\Todo;
 use Livewire\Component;
+use Livewire\Attributes\Rule;
 
 class TodoList extends Component
 {
+
+    #[Rule('required|min:3|max:50')]
+    
+    public $name;
+
+    public function create()  
+    {
+        // validate input
+        $validated = $this->validateOnly('name');
+
+        // create item
+        Todo::create($validated);
+
+        // clear input
+        $this->reset('name');
+
+        // send flash message
+        session()->flash('message', 'Item created successfully!');
+
+    }
+
     public function render()
     {
-        return view('livewire.todo-list');
+        $todos = Todo::latest()->get();
+        return view('livewire.todo-list', [
+            'todos' => $todos
+        ]);
     }
 }
